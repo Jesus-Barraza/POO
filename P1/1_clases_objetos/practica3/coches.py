a538027e1c15fa34031685e18341b10903f77324
Jesús Barraza <jesus_3141240166@utd.edu.mx>
2025-09-19 09:42:14 -0600

    Se ha actualizado la practica 3
    Wey noooooooooooooooooOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO

--- P1/1_clases_objetos/practica3/coches.py
+++ P1/1_clases_objetos/practica3/coches.py
@@ -1,4 +1,4 @@
-class Coches:
+'''class Coches:
     __marca=""
     __color=""
     __modelo=""
@@ -29,6 +29,41 @@ coche2._Coches__modelo="2020"
 coche2._Coches__velocidad=180
 coche2._Coches__caballaje=150
 coche2._Coches__plazas=6
+
+print(coche1.acelerar(100))
+print(coche2.frenar(150))'''
+
+class Coches:
+    marca=""
+    color=""
+    modelo=""
+    velocidad=0
+    caballaje=0
+    plazas=0
+    def acelerar(self,ace):
+        self.velocidad+=ace
+        return self.velocidad
+
+    def frenar(self,fre):
+        self.velocidad-=fre
+        return self.velocidad
+    
+coche1=Coches()
+coche2=Coches()
+
+coche1.marca="VW"
+coche1.color="Blanco"
+coche1.modelo="2022"
+coche1.velocidad=220
+coche1.caballaje=150
+coche1.plazas=5
+
+coche2.marca="Nissan"
+coche2.color="Azul"
+coche2.modelo="2020"
+coche2.velocidad=180
+coche2.caballaje=150
+coche2.plazas=6
 
 print(coche1.acelerar(100))
 print(coche2.frenar(150))
